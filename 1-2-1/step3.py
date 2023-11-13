3ef1148b45c50b151ce46779313f2703b60401ba
Alexandra <alexandra.kovacs@vtsd.com>
2023-11-13 08:41:27 -0500

    h

--- 1-2-1/step3.py
+++ 1-2-1/step3.py
@@ -9,13 +9,45 @@ shell_color = "pink"
 shell_size = 2
 shell_shape = "turtle"
 score = 0
+font_setup = ("Arial", 20, "normal")
 
 # -----initialize turtle-----
 shell = trtl.Turtle()
 shell.fillcolor(shell_color)
 shell.turtlesize(shell_size)
 shell.shape(shell_shape)
+score_writer = trtl.Turtle()
+score_writer.penup()
+score_writer.goto(400,350)
+counter =  trtl.Turtle()
+counter.penup()
+counter.goto(-400,350)
+
+#-----countdown variables-----
+font_setup = ("Arial", 20, "normal")
+timer = 5
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
+
+
 # -----game functions--------
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
+
+def update_score():
+    global score  # gives this function access to the score that was created above
+    score += 1
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
+
 def change_position():
     new_xpos = rand.randint(-300,300)
     new_ypos = rand.randint(-300,300)
@@ -27,10 +59,13 @@ def change_position():
 
 def shell_clicked(x,y):
     change_position()
+    update_score()
 
 #-----events----------------
 shell.onclick(shell_clicked)
 
 
+
 wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
