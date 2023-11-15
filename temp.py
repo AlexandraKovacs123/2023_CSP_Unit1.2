e3fce414777e19c5b3887c1554c67a1a8c10c228
Alexandra <alexandra.kovacs@vtsd.com>
2023-11-15 10:07:22 -0500

    h

---
+++ temp.py
@@ -0,0 +1,26 @@
+# Stage 1
+'''
+number = 1
+star = "*"
+'''
+'''
+for stars in range(5):
+    print(star * number)
+    number = number + 1
+'''
+
+
+# Bonus
+def stars():
+    spaces = " "
+    space =
+    num = 4
+    number = 1
+    star = "*"
+    for stars in range(5):
+        print(spaces * num)
+        print(star * number)
+        number = number + 1
+        num = num - 1
+
+stars()
