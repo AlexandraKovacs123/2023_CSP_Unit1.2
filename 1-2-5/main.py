ae1f5c835d686806a74918912387b5f8d7c3c41c
Alexandra <alexandra.kovacs@vtsd.com>
2024-01-08 10:11:08 -0500

    h

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -4,11 +4,25 @@ import turtle as trtl
 snake = trtl.Turtle()
 
 
-
-
-
-
-
 wn = trtl.Screen()
-wn.bgpic("snakeBg.png")
+wn.bgpic("snakeBg4.png")
+
+snake.color('blue')
+snake.penup()
+
+speed = 1
+
+def travel():
+    snake.forward(speed)
+    wn.ontimer(travel, 10)
+
+wn.onkey(lambda: snake.setheading(90), 'Up')
+wn.onkey(lambda: snake.setheading(180), 'Left')
+wn.onkey(lambda: snake.setheading(0), 'Right')
+wn.onkey(lambda: snake.setheading(270), 'Down')
+
+wn.listen()
+
+travel()
+
 wn.mainloop()
