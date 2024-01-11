06ca0d8923af3c895742ceee3487447111091d8a
Alexandra <alexandra.kovacs@vtsd.com>
2024-01-11 09:24:15 -0500

    h

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -1,46 +1,55 @@
-# import turtle & random
 import turtle as trtl
 import random as rand
 
-# Create turtles & images
-snake = trtl.Turtle()
-apples = "apple5.gif"
+
+#creating turtles
+apple_image = "apple5.gif"
 apple = trtl.Turtle()
+snake = trtl.Turtle()
 wn = trtl.Screen()
-wn.bgpic("snakeBg4.png")
-wn.addshape(apples)
-
-snake.color('blue')
+#set up snake and turtle
+wn.addshape(apple_image)
+apple.shape(apple_image)
+apple.hideturtle()
+apple.penup()
+apple.goto(50,0)
+apple.showturtle()
+wn.bgcolor("black")
+snake.color('red')
 snake.penup()
-apple.shape(apples)
-snake.turtlesize(2)
-speed = 1
-
-
-# turtle movement
+speed = .3
 def travel():
-    snake.forward(speed)
-    wn.ontimer(travel, 10)
-
-
+   snake.forward(speed)
+   wn.ontimer(travel, 1)
+   touch()
 wn.onkey(lambda: snake.setheading(90), 'Up')
 wn.onkey(lambda: snake.setheading(180), 'Left')
 wn.onkey(lambda: snake.setheading(0), 'Right')
 wn.onkey(lambda: snake.setheading(270), 'Down')
 
 
-def randLocate():
-    possible = [-175, -125, -75, -25, 25, 75, 125, 175]
-    locationX = rand.choice(possible)
-    locationY = rand.choice(possible)
-    apple.penup()
-    apple.hideturtle()
-    apple.goto(locationX, locationY)
-    apple.showturtle()
+def randomapple():
+   apple.penup()
+   possible = [-175, -125, -75, -25, 25, 75, 125, 175]
+   locationx = rand.choice(possible)
+   locationy = rand.choice(possible)
+   apple.hideturtle()
+   apple.goto(locationx, locationy)
+   apple.showturtle()
+def touch():
+    if abs(apple.xcor() - snake.xcor()) < 7:
+       if abs(apple.ycor() - snake.ycor()) < 7:
+           global speed
+           print("good")
+           randomapple()
+           speed += .1
+
+
+
 
 wn.listen()
-
 travel()
-randLocate()
-
+randomapple()
+wn = trtl.Screen()
+wn.bgpic("snakeBg4.png")
 wn.mainloop()
