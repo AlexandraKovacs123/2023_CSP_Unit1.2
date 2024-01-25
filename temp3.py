b8379ce26fb61eff197d3982f8c06c2ce6b61676
Alexandra <alexandra.kovacs@vtsd.com>
2024-01-25 08:41:08 -0500

    h

---
+++ temp3.py
@@ -0,0 +1,10 @@
+# write a method called addSubtract()
+# this method should take a parameter called operation
+# when method is called, ask the user for two numbers
+# do the operation of the parameter to the inputs
+
+def addSubtract(operation):
+    first = input("choose a number")
+    second = input("choose another number")
+    if operation == "add":
+        print(first)
